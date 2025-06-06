a336fe4a9b05ef45a3221de072d643f8a7228dec
Samantha <120396584+JessWoodhouse@users.noreply.github.com>
2025-06-06 14:15:18 +0100

    Integrate peLoader and Moffat.EoReader for EMF parsing

--- EmfMapViewer/EMFReader.cs
+++ EmfMapViewer/EMFReader.cs
@@ -1,147 +1,98 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using Moffat.EndlessOnline.SDK.Data;
 
 namespace EmfMapViewer
 {
     public class GFX
     {
-        public int X { get; set; }
-        public int Tile { get; set; }
-
-        public GFX(int x, int tile)
-        {
-            X = x;
-            Tile = tile;
-        }
+        public int X { get; }
+        public int Tile { get; }
+        public GFX(int x, int tile) { X = x; Tile = tile; }
     }
 
     public class GFXRow
     {
-        public int Y { get; set; }
-        public List<GFX> Tiles { get; set; }
-
-        public GFXRow(int y, List<GFX> tiles)
-        {
-            Y = y;
-            Tiles = tiles;
-        }
+        public int Y { get; }
+        public List<GFX> Tiles { get; }
+        public GFXRow(int y, List<GFX> tiles) { Y = y; Tiles = tiles; }
     }
 
     public class EMFReader
     {
         private readonly string _filename;
-        private byte[] _data;
-        private int _pos;
-
+        private EoReader _r;
         public int Width { get; private set; }
         public int Height { get; private set; }
         public int FillTile { get; private set; }
-        public List<List<GFXRow>> GfxRows { get; private set; }
+        public List<List<GFXRow>> GfxRows { get; } = new(9);
 
-        public EMFReader(string filename)
-        {
-            _filename = filename;
-            GfxRows = new List<List<GFXRow>>(9);
-        }
+        public EMFReader(string filename) => _filename = filename;
 
-        private int Eon1(int b)
+        int E1(int b) => (b = (b == 0 || b == 254 ? 1 : b)) - 1;
+        int E2(int b1, int b2)
         {
-            if (b == 0 || b == 254) b = 1;
-            return b - 1;
-        }
-
-        private int Eon2(int b1, int b2)
-        {
-            if (b1 == 0 || b1 == 254) b1 = 1;
-            if (b2 == 0 || b2 == 254) b2 = 1;
+            b1 = (b1 == 0 || b1 == 254 ? 1 : b1);
+            b2 = (b2 == 0 || b2 == 254 ? 1 : b2);
             return (b2 - 1) * 253 + (b1 - 1);
-        }
-
-        private byte[] ReadBytes(int n)
-        {
-            var chunk = new byte[n];
-            Array.Copy(_data, _pos, chunk, 0, n);
-            _pos += n;
-            return chunk;
         }
 
         public bool Load()
         {
             try
             {
-                _data = File.ReadAllBytes(_filename);
-                _pos = 0;
-
-                var hdr = ReadBytes(0x2E);
+                var data = File.ReadAllBytes(_filename);
+                _r = new EoReader(data);
+                var hdr = _r.GetBytes(0x2E);
                 if (hdr.Length < 3 || hdr[0] != (byte)'E' || hdr[1] != (byte)'M' || hdr[2] != (byte)'F')
                     return false;
 
-                Width    = Eon1(hdr[0x25]);
-                Height   = Eon1(hdr[0x26]);
-                FillTile = Eon2(hdr[0x27], hdr[0x28]);
+                Width    = E1(hdr[0x25]);
+                Height   = E1(hdr[0x26]);
+                FillTile = E2(hdr[0x27], hdr[0x28]);
 
-                _pos = 0x2E;
+                int n = E1(_r.GetByte()); _r.GetBytes(n * 8);
+                n = E1(_r.GetByte()); _r.GetBytes(n * 4);
+                n = E1(_r.GetByte()); _r.GetBytes(n * 12);
 
-                int npcCount = Eon1(_data[_pos]);
-                _pos += 1 + npcCount * 8;
-
-                int u1Count = Eon1(_data[_pos]);
-                _pos += 1 + u1Count * 4;
-
-                int chestCount = Eon1(_data[_pos]);
-                _pos += 1 + chestCount * 12;
-
-                int tileRowCount = Eon1(_data[_pos]);
-                _pos += 1;
-                for (int i = 0; i < tileRowCount; i++)
+                int rows = E1(_r.GetByte());
+                for (int i = 0; i < rows; i++)
                 {
-                    byte[] rh = ReadBytes(2);
-                    int cnt = Eon1(rh[1]);
-                    _pos += cnt * 2;
+                    var rh = _r.GetBytes(2);
+                    _r.GetBytes(E1(rh[1]) * 2);
                 }
 
-                int warpRowCount = Eon1(_data[_pos]);
-                _pos += 1;
-                for (int i = 0; i < warpRowCount; i++)
+                rows = E1(_r.GetByte());
+                for (int i = 0; i < rows; i++)
                 {
-                    byte[] rh = ReadBytes(2);
-                    int cnt = Eon1(rh[1]);
-                    _pos += cnt * 8;
+                    var rh = _r.GetBytes(2);
+                    _r.GetBytes(E1(rh[1]) * 8);
                 }
 
                 for (int layer = 0; layer < 9; layer++)
                 {
                     var layerRows = new List<GFXRow>();
-                    int outerSize = Eon1(_data[_pos]);
-                    _pos += 1;
-
-                    for (int r = 0; r < outerSize; r++)
+                    int count = E1(_r.GetByte());
+                    for (int r = 0; r < count; r++)
                     {
-                        byte[] rh = ReadBytes(2);
-                        int rowY = Eon1(rh[0]);
-                        int cnt  = Eon1(rh[1]);
-                        var tiles = new List<GFX>(cnt);
-
-                        for (int t = 0; t < cnt; t++)
+                        var rh = _r.GetBytes(2);
+                        int y = E1(rh[0]), c = E1(rh[1]);
+                        var tiles = new List<GFX>(c);
+                        for (int t = 0; t < c; t++)
                         {
-                            byte[] gb = ReadBytes(3);
-                            int gx  = Eon1(gb[0]);
-                            int tid = Eon2(gb[1], gb[2]);
-                            tiles.Add(new GFX(gx, tid));
+                            var gb = _r.GetBytes(3);
+                            tiles.Add(new GFX(E1(gb[0]), E2(gb[1], gb[2])));
                         }
-
-                        layerRows.Add(new GFXRow(rowY, tiles));
+                        layerRows.Add(new GFXRow(y, tiles));
                     }
-
                     GfxRows.Add(layerRows);
                 }
 
                 return true;
             }
-            catch (Exception ex)
+            catch
             {
-                Console.WriteLine($"Error loading EMF file {_filename}: {ex.Message}");
                 return false;
             }
         }
